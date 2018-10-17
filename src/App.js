6c56d7301d8d83ebf1b21ef3623830c935621941
MohsAkhtar <mohsin.akhtar0@gmail.com>
2018-10-17 17:22:56 +0100

    Changed robots to kittens!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
+import { kittens } from "./kittens";
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      kittens: kittens,
+      searchfield: ""
+    };
+  }
+
+  onSearchChange(event) {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList kittens={this.state.kittens} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/CardList.js
+++ src/CardList.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Card from "./Card";
 
-const CardList = ({ robots }) => {
+const CardList = ({ kittens }) => {
   return (
     <div>
-      {robots.map((user, i) => {
+      {kittens.map((user, i) => {
         return (
           <Card
             key={i}
-            id={robots[i].id}
-            name={robots[i].name}
-            email={robots[i].email}
+            id={kittens[i].id}
+            name={kittens[i].name}
+            email={kittens[i].email}
           />
         );
       })}
