701c78d39866c66e0e48389bad34d0b55faae83c
MohsAkhtar <mohsin.akhtar0@gmail.com>
2018-10-18 03:33:21 +0100

    Updates

---
+++ precache-manifest.52ec7469f26fcf1dbed2ba78264d172c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/project-react-kitten-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "0c850cd044e7c7a7a341",
+    "url": "/project-react-kitten-friends/static/js/runtime~main.0c850cd0.js"
+  },
+  {
+    "revision": "df89f559a6e38840b585",
+    "url": "/project-react-kitten-friends/static/js/main.df89f559.chunk.js"
+  },
+  {
+    "revision": "8cc1aab638174b752eb3",
+    "url": "/project-react-kitten-friends/static/js/1.8cc1aab6.chunk.js"
+  },
+  {
+    "revision": "df89f559a6e38840b585",
+    "url": "/project-react-kitten-friends/static/css/main.7adba760.chunk.css"
+  },
+  {
+    "revision": "8cc1aab638174b752eb3",
+    "url": "/project-react-kitten-friends/static/css/1.dc8fe82d.chunk.css"
+  },
+  {
+    "revision": "afe35055c90b0ea11e450f0b6c76ba64",
+    "url": "/project-react-kitten-friends/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/project-react-robo-friends/precache-manifest.87157ce60cbf9b0bab33f07ffda654a1.js"
+  "/project-react-kitten-friends/precache-manifest.52ec7469f26fcf1dbed2ba78264d172c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/project-react-robo-friends/index.html", {
+workbox.routing.registerNavigationRoute("/project-react-kitten-friends/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
